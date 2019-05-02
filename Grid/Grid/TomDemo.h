a89ab8185a06dd113e3e4f53d653c3a8dbd60963
Tom Bonner <zerosavant@yahoo.com>
2019-05-02 15:06:37 -0500

    reorganized tomdemo output

--- Grid/Grid/TomDemo.h
+++ Grid/Grid/TomDemo.h
@@ -4,11 +4,12 @@
 using namespace std;
 
 void addRowColDemo() {
+	cout << "\n************* addRowColDemo() ***********\n";
 	Grid<int> xGrid;
 	LinkedList<int> rowListA, rowListB, rowListC,
 		colListD, colListE;
 
-	cout << "************ Rows *************\n";
+	cout << "::: Rows \n";
 	rowListA.add(45); rowListA.add(12); rowListA.add(67);
 	rowListA.add(14); rowListA.add(32);	rowListA.add(29);
 
@@ -34,7 +35,7 @@ void addRowColDemo() {
 	cout << "Size: " << xGrid.getSize() << endl;
 	printGrid(xGrid);
 
-	cout << "\n************ Columns *************\n";
+	cout << "\n::: Columns\n";
 	colListD.add(120);
 	colListD.add(141);
 	colListD.add(192);
@@ -58,7 +59,9 @@ void addRowColDemo() {
 };
 
 void llConstructorDemo() {
+	cout << "\n************ llConstructorDemo() *********";
 	LinkedList<int> xList;
+	cout << endl << endl;
 
 	xList.add(23);
 	xList.add(14);
@@ -73,25 +76,26 @@ void llConstructorDemo() {
 	xList.add(25);
 	xList.add(9);
 
-	Grid<int> xGrid(xList,4,3);
+	cout << "creating someGrid:\n";
+	Grid<int> someGrid(xList,4,3);
 
-	printGrid(xGrid);
+	printGrid(someGrid);
 
 	cout << endl;
-	cout << "Has 99? (expect 0): " << xGrid.contains(99) << endl;
-	cout << "Has 37? (expect 1): " << xGrid.contains(37) << endl;
-	cout << "Has 50? (expect 0): " << xGrid.contains(50) << endl;
-	cout << "Has 17? (expect 0): " << xGrid.contains(17) << endl;
-	cout << "Has 25? (expect 1): " << xGrid.contains(25) << endl;
-	cout << "Has 14? (expect 1): " << xGrid.contains(14) << endl;
-	cout << "Has 82? (expect 0): " << xGrid.contains(82) << endl;
+	cout << "Has 99? (expect 0): " << someGrid.contains(99) << endl;
+	cout << "Has 37? (expect 1): " << someGrid.contains(37) << endl;
+	cout << "Has 50? (expect 0): " << someGrid.contains(50) << endl;
+	cout << "Has 17? (expect 0): " << someGrid.contains(17) << endl;
+	cout << "Has 25? (expect 1): " << someGrid.contains(25) << endl;
+	cout << "Has 14? (expect 1): " << someGrid.contains(14) << endl;
+	cout << "Has 82? (expect 0): " << someGrid.contains(82) << endl;
 
-	cout << "\nIs Empty? " << xGrid.isEmpty();
+	cout << "\nIs Empty? " << someGrid.isEmpty();
 
 
 }
 
 void runTomDemo() {
-	//addRowColDemo();
+	addRowColDemo();
 	llConstructorDemo();
 }
